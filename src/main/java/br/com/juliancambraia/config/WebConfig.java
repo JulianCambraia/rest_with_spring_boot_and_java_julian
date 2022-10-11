33c7d9d84f924e1a1bf7eb8a560bb1018b69e62d
JulianCambraia <juliancambraia@gmail.com>
2022-10-10 22:44:51 -0300

    Implementa Content negotiation YAML via Header Param

--- src/main/java/br/com/juliancambraia/config/WebConfig.java
+++ src/main/java/br/com/juliancambraia/config/WebConfig.java
@@ -1,15 +1,21 @@
 package br.com.juliancambraia.config;
 
+import br.com.juliancambraia.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * * Content negotiation via Header Param localhost:8080/api/person/v1 (xml ou json) setando o Accept nos Headers do Postman
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -18,6 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
     }
 }
